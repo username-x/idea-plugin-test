35da28017252232eec26bfe6292d5e9beac84d3b
GitHub Action <action@github.com>
2025-10-02 12:22:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/usernamex/ideaplugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/usernamex/ideaplugintest/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.usernamex.ideaplugintest.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.usernamex.ideaplugintest.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/usernamex/ideaplugintest/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/usernamex/ideaplugintest/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.usernamex.ideaplugintest.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
